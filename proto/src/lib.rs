72e0ed8c0077ad811712dc584db2abd1c46b8877
mowzhja <mowzhja@protonmail.ch>
2021-06-25 22:22:09 +0200

    initial protocol code

--- proto/src/lib.rs
+++ proto/src/lib.rs
@@ -1,3 +1,20 @@
+/// Struct containing all the data relating to a single packet.
+#[derive(Debug)]
+struct Packet {
+    raw: Vec<u8>,
+    timeout: u32,
+    cflag: bool, // connection flag (connection-oriented vs connectionless)
+    data: String, // the actual message
+    mac: String, // message authentication code
+}
+
+impl Packet {
+    pub fn new(stream: Vec<u8>) -> Self {
+        todo!();
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
