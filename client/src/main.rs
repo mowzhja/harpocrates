670de409107a26ed847fb12cca885102e2a36367
mowzhja <mowzhja@protonmail.ch>
2021-06-27 18:21:19 +0200

    added initial client code

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,20 @@
+use std::net::TcpStream;
+use rand_core::OsRng;
+use x25519_dalek::{EphemeralSecret, PublicKey};
+
 fn main() {
-    println!("Hello, world!");
+    let stream = TcpStream::connect("127.0.0.1:9001").unwrap();
+
+    let client_secret = EphemeralSecret::new(OsRng);
+    let client_public = PublicKey::from(&client_secret);
+
+    // send pubkey to server
+
+    // get server's pubkey
+    let server_public = get_server_pubkey();
+
+    let shared = client_secret.diffie_hellman(&client_public);
+    
+    // for debugging puproses only!
+    println!("{:?}", shared.as_bytes());
 }
--- server/src/lib.rs
+++ server/src/lib.rs
@@ -5,8 +5,14 @@ use x25519_dalek::{EphemeralSecret, PublicKey};
 pub fn establish_key() {
     let server_secret = EphemeralSecret::new(OsRng);
     let server_public = PublicKey::from(&server_secret);
+    
+    // send pubkey to client
 
+    // get client's pubkey
     let client_public = get_client_pubkey();
 
     let shared = server_secret.diffie_hellman(&client_public);
+    
+    // for debugging puproses only!
+    println!("{:?}", shared.as_bytes());
 }
